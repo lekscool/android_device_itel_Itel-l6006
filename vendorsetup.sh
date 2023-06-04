e0ad0cbc67f1d3de97528ac208a15fbb8a1c26ad
lekscool <130257253+lekscool@users.noreply.github.com>
2023-06-04 10:21:31 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_itel-L6006-user
-add_lunch_combo omni_itel-L6006-userdebug
-add_lunch_combo omni_itel-L6006-eng
+#add_lunch_combo omni_itel-L6006-user
+#add_lunch_combo omni_itel-L6006-userdebug
+#add_lunch_combo omni_itel-L6006-eng
